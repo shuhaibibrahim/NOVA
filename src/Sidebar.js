b8cfc45e05ff17337d5b34d2d02cebdf7ea93ffd
shuhaibibrahim <52924174+shuhaibibrahim@users.noreply.github.com>
2026-08-26 11:24:04 +0530

    Rename Data Entry to Article Entry

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -128,7 +128,7 @@ function Sidebar({spareData, selectedLink, setSelectedLink, openedTab, setOpened
                     [
                         {
                             to:"admin/data-entry",
-                            label:"Data Entry",
+                            label:"Article Entry",
                             icon:(<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
                                 </svg>)

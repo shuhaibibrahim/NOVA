60dd9743d8e72a94e64575828a1eeec43b7fd776
shuhaibibrahim <52924174+shuhaibibrahim@users.noreply.github.com>
2026-08-28 17:31:27 +0530

    Add SFG Store menu to MM Department

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -220,6 +220,36 @@ function Sidebar({spareData, selectedLink, setSelectedLink, openedTab, setOpened
                                     </svg>)
                                 }
                             ]
+                        },
+                        {
+                            value: "sfgStore",
+                            label: "SFG Store",
+                            icon: (<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7h18M5 7v12h14V7M9 11h6" />
+                            </svg>),
+                            children: [
+                                {
+                                    to:"mmdept/clicker-comp-store",
+                                    label:"Clicker Comp Store",
+                                    icon:(<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M12 22a10 10 0 100-20 10 10 0 000 20z" />
+                                    </svg>)
+                                },
+                                {
+                                    to:"mmdept/printing-comp-store",
+                                    label:"Printing Comp Store",
+                                    icon:(<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M12 22a10 10 0 100-20 10 10 0 000 20z" />
+                                    </svg>)
+                                },
+                                {
+                                    to:"mmdept/molding-receiving-store",
+                                    label:"Molding Receiving Store",
+                                    icon:(<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M12 22a10 10 0 100-20 10 10 0 000 20z" />
+                                    </svg>)
+                                }
+                            ]
                         }
                     ])
             }
